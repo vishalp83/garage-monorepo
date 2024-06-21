1d809ee15eab00ce7fb6887f926aa3f7c2525bd4
Vishal Patel <vishalp83@gmail.com>
2024-06-20 20:55:06 -0400

    add env to garage api

--- apps/garage-api/src/handlers/user.ts
+++ apps/garage-api/src/handlers/user.ts
@@ -1,7 +1,9 @@
 import { Handler, HttpVerb } from "@garage/ts-express-api";
 import { AuthTokenPayload, UserLookupInputModel, UserLookupResponseModel } from "../models";
-import { ValidationChain } from "express-validator";
-import { param, query } from "express-validator";
+// import { ValidationChain, query } from "express-validator";
+import { param } from "express-validator";
+import { PrismaClient } from '@prisma/client'
+import { User, UserRepository } from "@garage/database";
 
 
 
@@ -15,13 +17,35 @@ export class UserHandler extends Handler <
     readonly path: string = "/user/:UserId";
     validations = [param("UserId").trim()];
     override requiresAuth = false;
+    prisma = new PrismaClient()
 
     async handler({UserId}: UserLookupInputModel, auth: AuthTokenPayload): Promise<UserLookupResponseModel> {
+        const test = new UserRepository();
+        const user: User = await test.findByEmail("vishalp83@gmail.com");
 
-        return {
-            "UserId": UserId,
-            "FirstName": "Vishal",
-            "LastName": "Patel" 
+        // const user = await this.prisma.user.findUnique({
+        //     where: {email: "vishalp83@gmail.com"},
+        //     // include: {
+        //     //     vehicles: true,
+        //     //     mods: true,
+        //     //     glove_box: true
+        //     // }
+        // })
+
+        console.log(user);
+
+        if(user) {
+            return {
+                UserId: user.id.toString(),
+                FirstName: user.first_name,
+                LastName: user.last_name ?? ""
+            }
+        } else {
+            return {
+                UserId: "",
+                FirstName: "",
+                LastName: ""
+            }
         }
     }
 
